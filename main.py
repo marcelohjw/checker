3b6945244cd14d71c9827614fc95cfa8475033b5
Marcelo Júnior <marcelohjw@gmail.com>
2021-05-23 16:02:41 -0300

    Error getting the images..

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+from PIL import Image
+from io import BytesIO
 
 
 """
@@ -14,10 +16,19 @@ import numpy as np
 search = input('You want to create a database of?: ')
 params = {"q": search}
 
-r = requests.get("http://www.bing.com/search", params=params)
+r = requests.get("https://www.bing.com/images/search", params)
 print("Status: ", r.status_code)
+
 soup = BeautifulSoup(r.text, "html.parser")
-print(soup.prettify())
+links = soup.find_all("a", {"class": "iusc"})
+print("Results:", len(links))
+
+for item in links:
+    img_obj = requests.get(item.attrs["href"])
+    print("Getting", item.attrs["href"])
+    title = item.attrs["href"].split("/")[-1]
+    img = Image.open(BytesIO(img_obj.content))
+    img.save("./sets/" + title, img.format)
 
 '''
 while True:
