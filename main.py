3934a9153980435a7ec5dce151614468be22d0be
Marcelo Júnior <marcelohjw@gmail.com>
2021-05-23 14:59:29 -0300

    Implemented beautiful soup to get images

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+
 """
 import cv2
 import numpy as np
 """
-import requests
 
 
 # This is how i can capture video from the webcam.
 # cap = cv2.VideoCapture(0)
 
-r = requests.get('https://google.com.br')
+search = input('You want to create a database of?: ')
+params = {"q": search}
+
+r = requests.get("http://www.bing.com/search", params=params)
 print("Status: ", r.status_code)
+soup = BeautifulSoup(r.text, "html.parser")
+print(soup.prettify())
 
 '''
 while True:
