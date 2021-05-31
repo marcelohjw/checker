b34d0d92a49932cfe19a70c6bc87a7ef2b1f62ce
Marcelo Júnior <marcelohjw@gmail.com>
2021-05-31 20:46:53 -0300

    Started using PySimpleGUI

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import PySimpleGUI as sg
 from bs4 import BeautifulSoup
 from PIL import Image
 from io import BytesIO
 import requests
 
 search_url = 'https://bing.com/images/search'
-keyword = input("Type what you want to get: ")
+keyword = sg.popup_get_text('What you want to get?')
 params = {'q': '+' + keyword}
 
 r = requests.get(search_url, params)
@@ -13,7 +14,7 @@ soap = BeautifulSoup(r.text, 'html.parser')
 
 images = soap.find_all('img', {'class': 'mimg'})
 numpery = len(images)
-print("Getting " + str(numpery) + " " + keyword + " images...")
+sg.popup_annoying("Getting " + str(numpery) + " " + keyword + " images...")
 
 count = 1
 for img in images:
@@ -24,5 +25,4 @@ for img in images:
     format4 = img.format
     img.save(f'images/{keyword}{count}.{format4.lower()}', img.format)
     count += 1
-    if count == 10:
-        print("Finished!")
+sg.popup_annoying("Finished! Check images folder!", background_color='Green')
